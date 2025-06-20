72fff597c81cec939692eb2fa91e6d64b87979d3
Aditya maurya <118655919+Adityamaurya76@users.noreply.github.com>
2025-06-20 18:08:09 +0530

    Update Contact.js

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -130,7 +130,7 @@ const Contact = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-   emailjs.sendForm('service_w9pnbuw','template_drkcemh', form.current,'luMc5kjzk2VZrE3tl')
+   emailjs.sendForm('service_hnf6gu8','template_drkcemh', form.current,'luMc5kjzk2VZrE3tl')
       .then((result) => {
         setOpen(true);
         form.current.reset();
